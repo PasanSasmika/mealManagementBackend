3a7a7f637e5549c2b04207139bacf9beda60d74a
PasanSasmika <pasansasmika198@gmail.com>
2026-01-13 18:34:18 +0530

    token expire

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -8,7 +8,7 @@ export class AuthService {
     return jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET!,
-      { expiresIn: '1d' }
+      { expiresIn: '90d' }
     );
   }
 
